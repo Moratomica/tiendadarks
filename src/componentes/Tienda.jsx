0992fc7083d458ef1a038ebb20765330da447849
Moratomica <paulina.mora.be@gmail.com>
2022-11-01 20:35:19 -0600

    Update Tienda.jsx

--- src/componentes/Tienda.jsx
+++ src/componentes/Tienda.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import img1 from '../assets/img1.png'
-import img2 from '../assets/img2.png'
-import img3 from '../assets/img3.png'
-import img4 from '../assets/img4.png'
-import img5 from '../assets/img5.png'
-import img6 from '../assets/img6.png'
+import img10 from '../assets/img1.png'
+import img20 from '../assets/img2.png'
+import img30 from '../assets/img3.png'
+import img40 from '../assets/img4.png'
+import img50 from '../assets/img5.png'
+import img60 from '../assets/img6.png'
 
 function Tienda() {
   return (
@@ -15,27 +15,27 @@ function Tienda() {
             </div>
         <div className='grid grid-cols-2 gap-4 mx-4 xl:grid-cols-6 xl:justify-center xl:items-center lg:grid-cols-3'>
             <div>
-                <img src={img1} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
+                <img src={img10} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
             </div>
 
             <div>
-                <img src={img2} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
+                <img src={img20} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
             </div>
 
             <div>
-                <img src={img3} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
+                <img src={img30} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
             </div>
 
             <div>
-                <img src={img4} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
+                <img src={img40} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
             </div>
 
             <div>
-                <img src={img5} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
+                <img src={img50} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
             </div>
 
             <div>
-                <img src={img6} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
+                <img src={img60} className='mx-0 rounded-xl brightness-50 hover:brightness-100 cursor-pointer xl:h-[282px]' />
             </div>
 
         </div>
